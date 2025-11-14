d16bd9592fcbda4de5bd16ef7b14587de5ca1f5a
oliver könig <okoenig@nvidia.com>
2025-11-14 12:12:19 +0000

    fix
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- transformer_engine/pytorch/setup.py
+++ transformer_engine/pytorch/setup.py
@@ -86,10 +86,8 @@ def get_wheel_url():
     torch_cuda_version = parse(torch.version.cuda)
     # For CUDA 11, we only compile for CUDA 11.8, and for CUDA 12 we only compile for CUDA 12.3
     # to save CI time. Minor versions should be compatible.
-    if torch_cuda_version.major == 11:
-        torch_cuda_version = parse("11.8")
-    elif torch_cuda_version.major == 12:
-        torch_cuda_version = parse("12.3")
+    if torch_cuda_version.major == 12:
+        torch_cuda_version = parse("12.")
     elif torch_cuda_version.major == 13:
         torch_cuda_version = parse("13.0")
     else:
